bcc1abdd967bb592edd444739e5ebf002f5aa324
Michael Anderson <michanders@msn.com>
2018-02-28 23:29:36 +0000

    automatically backed up by learn

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
-export default class Address extends React.Component {
-  constructor(props) {
-    super(props);
- 
+export default class ImageSlider extends React.Component {
+  constructor() {
+    super();
+
     this.state = {
       currentSlideIndex: 0
     }
+  }
+
+  render(){
+    return (
+      <h3>I am on slide [this.state.currentSlideIndex]</h3>
+    )
   }
 }
