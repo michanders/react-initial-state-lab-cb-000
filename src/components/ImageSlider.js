3c628448a074636502ab2e820dc0a25d0b60c6ad
Michael Anderson <michanders@msn.com>
2018-02-28 23:26:10 +0000

    automatically backed up by learn

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,1 +1,11 @@
-// Bomb Component Code Goes Here
+import React from 'react';
+
+export default class Address extends React.Component {
+  constructor(props) {
+    super(props);
+ 
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+}
